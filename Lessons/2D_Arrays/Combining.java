7895794af260c4ea933679cd4b6c212ffa5dd1e6
Cole Webster <CWebster_Coding@outlook.com>
2023-02-09 17:24:13 -0500

    Updating

--- Lessons/2D_Arrays/Combining.java
+++ Lessons/2D_Arrays/Combining.java
@@ -10,14 +10,30 @@ public class Combining {
     };
 
     //First, we want to crop the image down to a 4x6 image, removing the right 2 columns. Declare and initialize a new 2D array of integers with 4 rows and 6 columns called `newImage`.
-    int[][] newImage = null;
+    int[][] newImage = new int[4][6];
 
     //Now that you have your empty image, use nested **for** loops to copy over the data from the original image to the new image, make sure not to include the cropped out columns.
+    for(int i = 0; i < newImage.length; i++) {
+      for(int j = 0; i < newImage[i].length; j++) {
+        newImage[i][j] = imageData[i][j];
+      }
+    }
 
     System.out.println(Arrays.deepToString(newImage));
 
     //You want to decrease the brightness of the new image by 50 units. The way this works is that for every integer in the new 2D array, we will subtract the value by 50. Remember that the value range for the pixel is 0-255, so if the result tries to go below 0, just set it equal to 0.
-
+    for(int i = 0; i < newImage.length; i++) {
+      for(int j = 0; j < newImage[i].length; j++) {
+        if(newImage[i][j]-50 < 0){
+          newImage[i][j] = 0;
+        }
+        else{
+          newImage[i][j]-=50;
+        }
+      }
+    }
+    //  Remember to check if the value minus 50 is less than 0 when iterating through the elements of the new image: if(newImage[row][column]-50<0). If that condition is true, then set the element to equal 0 else subtract 50 from the element.
+    
     System.out.println(Arrays.deepToString(newImage));
   }
 }
